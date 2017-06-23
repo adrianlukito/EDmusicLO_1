1c159a107ce0f329ab1d12a84e8d531b9b54d349
Adrian Luktio Lo <lukito.adrian@yahoo.com>
2017-06-23 13:33:42 +0700

    Nambahin Friend Activity

--- app/src/main/java/edu/bluejack16_2/edmusiclo/FriendActivity.java
+++ app/src/main/java/edu/bluejack16_2/edmusiclo/FriendActivity.java
@@ -1,45 +1,56 @@
 package edu.bluejack16_2.edmusiclo;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class FriendActivity extends AppCompatActivity {
 
     Drawable pic1, pic2, pic3;
 
+    EditText searchText;
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
 
+        //searchView = (SearchView) findViewById(R.id.searchFriend);
+//        ((EditText) searchView.findViewById(R.id.searchFriend)).setHintTextColor(Color.WHITE);
+//        ((EditText) searchView.findViewById(R.id.searchFriend)).setTextColor(Color.WHITE);
+//        ((EditText) searchView.findViewById(R.id.searchFriend)).setHint("Search Friend");
+
         ListView friendListView = (ListView) findViewById(R.id.friendListView);
         final FriendListViewAdapter friendListViewAdapter = new FriendListViewAdapter(getApplicationContext());
 
         pic1 = getResources().getDrawable(R.drawable.logo16_2v2);
-        pic2 = getResources().getDrawable(R.drawable.nav_drawer_wallpaper2);
+        pic2 = getResources().getDrawable(R.drawable.signup);
         pic3 = getResources().getDrawable(R.drawable.toolbar_wallpaper);
 
-        friendListViewAdapter.addFriendList("Adrian Lukito Lo",pic1);
-        friendListViewAdapter.addFriendList("Tedy Junaidi",pic1);
-        friendListViewAdapter.addFriendList("Albert Zhuang",pic1);
-        friendListViewAdapter.addFriendList("Alven Gemini Julio",pic1);
-        friendListViewAdapter.addFriendList("Bagas Prakoso",pic1);
-        friendListViewAdapter.addFriendList("Christian Adianto",pic1);
-        friendListViewAdapter.addFriendList("Evy Maria",pic1);
-        friendListViewAdapter.addFriendList("Giovan Saputra",pic1);
-        friendListViewAdapter.addFriendList("Jordan Leonardi",pic1);
-        friendListViewAdapter.addFriendList("Kelvin Asclepius Minor",pic1);
-        friendListViewAdapter.addFriendList("Luciana Dian Santami",pic1);
-        friendListViewAdapter.addFriendList("Misita Pontiasa",pic1);
-        friendListViewAdapter.addFriendList("Mario Viegash",pic1);
-        friendListViewAdapter.addFriendList("Philip Andreas Nugraha",pic1);
-        friendListViewAdapter.addFriendList("Peter Hartawan Suherman",pic1);
-        friendListViewAdapter.addFriendList("Renaldi",pic1);
-        friendListViewAdapter.addFriendList("Kevin Surya Wahyudi",pic1);
-        friendListViewAdapter.addFriendList("Thomas Asril",pic1);
-        friendListViewAdapter.addFriendList("Wimpi Jonathan",pic1);
+        friendListViewAdapter.addFriendList("Adrian Lukito Lo",pic2);
+        friendListViewAdapter.addFriendList("Tedy Junaidi",pic2);
+        friendListViewAdapter.addFriendList("Albert Zhuang",pic2);
+        friendListViewAdapter.addFriendList("Alven Gemini Julio",pic2);
+        friendListViewAdapter.addFriendList("Bagas Prakoso",pic2);
+        friendListViewAdapter.addFriendList("Christian Adianto",pic2);
+        friendListViewAdapter.addFriendList("Evy Maria",pic2);
+        friendListViewAdapter.addFriendList("Giovan Saputra",pic2);
+        friendListViewAdapter.addFriendList("Jordan Leonardi",pic2);
+        friendListViewAdapter.addFriendList("Kelvin Asclepius Minor",pic2);
+        friendListViewAdapter.addFriendList("Luciana Dian Santami",pic2);
+        friendListViewAdapter.addFriendList("Misita Pontiasa",pic2);
+        friendListViewAdapter.addFriendList("Mario Viegash",pic2);
+        friendListViewAdapter.addFriendList("Philip Andreas Nugraha",pic2);
+        friendListViewAdapter.addFriendList("Peter Hartawan Suherman",pic2);
+        friendListViewAdapter.addFriendList("Renaldi",pic2);
+        friendListViewAdapter.addFriendList("Kevin Surya Wahyudi",pic2);
+        friendListViewAdapter.addFriendList("Thomas Asril",pic2);
+        friendListViewAdapter.addFriendList("Wimpi Jonathan",pic2);
 
         friendListView.setAdapter(friendListViewAdapter);
     }
