c5d439502d912b12804c8233a14f2732ae4b2bb7
ThunderBi <TedyJUnaidi@ymail.com>
2017-07-11 16:31:04 +0700

    ebelum rev1

--- app/src/main/java/edu/bluejack16_2/edmusiclo/ArtistDetailActivity.java
+++ app/src/main/java/edu/bluejack16_2/edmusiclo/ArtistDetailActivity.java
@@ -1,12 +1,19 @@
 package edu.bluejack16_2.edmusiclo;
 
+import android.database.Cursor;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import edu.bluejack16_2.edmusiclo.model.MusicCursor;
 
 public class ArtistDetailActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -31,25 +38,63 @@ public class ArtistDetailActivity extends AppCompatActivity implements View.OnCl
         ListView artistSongListView = (ListView) findViewById(R.id.artistSongListView);
 
         final ArtistSongListViewAdapter artistSongListViewAdapter = new ArtistSongListViewAdapter(getApplicationContext());
+        try {
+            String[] proj = {MediaStore.Audio.Media._ID,
+                    MediaStore.Audio.Media.DATA,
+                    MediaStore.Audio.Media.DISPLAY_NAME,
+                    MediaStore.Video.Media.SIZE,
+                    MediaStore.Audio.Media.ALBUM,
+                    MediaStore.Audio.Media.ARTIST,
+                    MediaStore.Audio.Media.TITLE,
+                    MediaStore.Audio.Media.DURATION,
+                    MediaStore.Audio.Media.ALBUM_ID,
+                    MediaStore.Audio.Media.ARTIST_ID};
+            Log.d("text",getIntent().getExtras().getString("artist_id"));
+            String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0 AND " + MediaStore.Audio.Media.DATA + " Like '%.mp3' and "
+                    + MediaStore.Audio.Media.ARTIST + " = '" + getIntent().getExtras().getString("artist")+"'";
 
-        artistSongListViewAdapter.addArtistSongList("24K Magic (R3hab Remix)", "24K Magic (R3hab Remix)");
-        artistSongListViewAdapter.addArtistSongList("Versace On The Floor","Versace On The Floor (Bruno Mars vs. David)");
-        artistSongListViewAdapter.addArtistSongList("That's What I Like","That's What I Like (Alan Walker Remix)");
-        artistSongListViewAdapter.addArtistSongList("Uptown Funk","Uptown Funk");
-        artistSongListViewAdapter.addArtistSongList("Treasure","Summer Songs");
-        artistSongListViewAdapter.addArtistSongList("It Will Rain","Acoustic Rain");
-        artistSongListViewAdapter.addArtistSongList("When I Was Your Man","Now That's What I Call 30 Years");
-        artistSongListViewAdapter.addArtistSongList("Lock Out Of Heaven","Wine & Chocolates");
-        artistSongListViewAdapter.addArtistSongList("Just The Way You Are","Mum");
-        artistSongListViewAdapter.addArtistSongList("Nothin' On You","R&B Anthems");
-        artistSongListViewAdapter.addArtistSongList("The Lazy Song","The Lazy Song");
-        artistSongListViewAdapter.addArtistSongList("Grenade","The Grenade Sessions");
-        artistSongListViewAdapter.addArtistSongList("Talking To The Moon","Doo-Wops & Hooligans");
-        artistSongListViewAdapter.addArtistSongList("Count On Me","Doo-Wops & Hooligans");
+            final Cursor musicCursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    proj, selection, null, MediaStore.Audio.Media.TITLE + " ASC");
+
+            while (musicCursor.moveToNext()) {
+                artistSongListViewAdapter.addArtistSongList(musicCursor.getString(6), musicCursor.getString(4));
+            }
+        }catch (Exception e){
+            Log.d("text",e.toString());
+        }
+//        artistSongListViewAdapter.addArtistSongList("24K Magic (R3hab Remix)", "24K Magic (R3hab Remix)");
+//        artistSongListViewAdapter.addArtistSongList("Versace On The Floor","Versace On The Floor (Bruno Mars vs. David)");
+//        artistSongListViewAdapter.addArtistSongList("That's What I Like","That's What I Like (Alan Walker Remix)");
+//        artistSongListViewAdapter.addArtistSongList("Uptown Funk","Uptown Funk");
+//        artistSongListViewAdapter.addArtistSongList("Treasure","Summer Songs");
+//        artistSongListViewAdapter.addArtistSongList("It Will Rain","Acoustic Rain");
+//        artistSongListViewAdapter.addArtistSongList("When I Was Your Man","Now That's What I Call 30 Years");
+//        artistSongListViewAdapter.addArtistSongList("Lock Out Of Heaven","Wine & Chocolates");
+//        artistSongListViewAdapter.addArtistSongList("Just The Way You Are","Mum");
+//        artistSongListViewAdapter.addArtistSongList("Nothin' On You","R&B Anthems");
+//        artistSongListViewAdapter.addArtistSongList("The Lazy Song","The Lazy Song");
+//        artistSongListViewAdapter.addArtistSongList("Grenade","The Grenade Sessions");
+//        artistSongListViewAdapter.addArtistSongList("Talking To The Moon","Doo-Wops & Hooligans");
+//        artistSongListViewAdapter.addArtistSongList("Count On Me","Doo-Wops & Hooligans");
 
         artistSongListView.setAdapter(artistSongListViewAdapter);
     }
+    void playingSong(int position){
 
+        MusicCursor.getInstance().musiccursor.moveToPosition(position);
+        String path =  MusicCursor.getInstance().musiccursor.getString(1);
+        try {
+            if (SongFragment.mediaPlayer != null) {
+                SongFragment.mediaPlayer.stop();
+            }
+            SongFragment.mediaPlayer = new MediaPlayer();
+            SongFragment.mediaPlayer.setDataSource(path);
+            SongFragment.mediaPlayer.prepare();
+        }catch (Exception e){
+
+        }
+        SongFragment.mediaPlayer.start();
+    }
     @Override
     public void onClick(View view) {
         if(view == btnBackArtistDetail){
--- app/src/main/java/edu/bluejack16_2/edmusiclo/GenreActivity.java
+++ app/src/main/java/edu/bluejack16_2/edmusiclo/GenreActivity.java
@@ -1,14 +1,23 @@
 package edu.bluejack16_2.edmusiclo;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Typeface;
+import android.graphics.drawable.VectorDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
+
+import java.util.Vector;
+
+import edu.bluejack16_2.edmusiclo.model.MusicCursor;
 
 import static edu.bluejack16_2.edmusiclo.R.id.tvDiscoverAlbumTitle;
 
@@ -17,6 +26,8 @@ public class GenreActivity extends AppCompatActivity implements View.OnClickList
     Button backGenre, btnGenres;
 
     TextView tvDiscoverGenreTitle;
+
+    String sameText ="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +49,45 @@ public class GenreActivity extends AppCompatActivity implements View.OnClickList
 
         final GenreListViewAdapter genreListViewAdapter = new GenreListViewAdapter(getApplicationContext());
 
-        genreListViewAdapter.addGenre("Kpop");
-        genreListViewAdapter.addGenre("Rock");
-        genreListViewAdapter.addGenre("Pop");
-        genreListViewAdapter.addGenre("Jazz");
-        genreListViewAdapter.addGenre("EDM");
-        genreListViewAdapter.addGenre("Hip-Hop");
-        genreListViewAdapter.addGenre("Rnb");
-        genreListViewAdapter.addGenre("Acoustic");
-        genreListViewAdapter.addGenre("Blues");
-        genreListViewAdapter.addGenre("Soul");
-        genreListViewAdapter.addGenre("Punk");
-        genreListViewAdapter.addGenre("Funk");
-        genreListViewAdapter.addGenre("Country");
-        genreListViewAdapter.addGenre("Intrumental");
-        genreListViewAdapter.addGenre("Classic");
-        genreListViewAdapter.addGenre("Indie");
+
+        try {
+//            if (MusicCursor.getInstance().genreCursor == null) {
+//                MusicCursor.getInstance().setGenreCursor(this, "");
+//                Log.d("text", "error1");
+//            }
+//
+            Vector<String> test = new Vector<String>();
+//
+//            for(int i = 0;i < MusicCursor.getInstance().genreString.size(); i++){
+//                Log.d("text", MusicCursor.getInstance().genreString.get(i));
+//                int validasi = 0;
+//                for(int j = 0 ; j < test.size(); j++){
+//                    if (MusicCursor.getInstance().genreString.get(i).equals(test.get(j))){
+//                        validasi = 1;
+//                        break;
+//                    }
+//                }
+//                if(validasi == 0){
+//                    genreListViewAdapter.addGenre(MusicCursor.getInstance().genreString.get(i));
+//                    test.add(MusicCursor.getInstance().genreString.get(i));
+//                }
+//            }
+            Cursor musiCursor = MusicCursor.getInstance().musiccursor;
+            musiCursor.moveToFirst();
+            do {
+//                Uri uri = MediaStore.Audio.Genres.getContentUriForAudioId("external", Integer.parseInt(musiCursor.getString(0)));
+//
+//                condi = MediaStore.Audio.Genres.NAME + " = " + condition;
+//                genreCursor = activity.getContentResolver().query(uri, new String[]{MediaStore.Audio.Genres.NAME,
+//                        MediaStore.Audio.Genres._ID}, condi, null, MediaStore.Audio.Genres.NAME + " ASC");
+//
+//                while(genreCursor.moveToNext()) {
+//                    genreString.add(genreCursor.getString(genreCursor.getPosition()));
+//                }
+            }while (true);
+        }catch (Exception e){
+            Log.d("text", toString());
+        }
 
         genreListView.setAdapter(genreListViewAdapter);
     }
